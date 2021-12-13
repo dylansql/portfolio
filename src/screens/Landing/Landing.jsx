e522988f96d5e94b840c343b6e8c711057a5c266
koji <dylanbogerr@outlook.com>
2021-12-13 10:12:18 -0500

    Landing page update typewriter addition

--- src/screens/Landing/Landing.jsx
+++ src/screens/Landing/Landing.jsx
@@ -1,4 +1,5 @@
 import './Landing.css'
+import Typewriter from 'typewriter-effect'
 
 export default function Landing() {
     return (
@@ -6,7 +7,22 @@ export default function Landing() {
             <div className="intro-page">
                 <h1>Hi. Im Dylan!</h1>
                 <h1>Full Stack Engineer</h1>
-                <h2>A little About Me:</h2>
+                <h1>A little About Me: </h1>
+                <Typewriter 
+                className="typer"
+                onInit={(Typewriter) => {
+                    Typewriter.typeString("Full Stack Engineer")
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .typeString("Musician")
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .typeString("Artist")
+                    .pauseFor(2000)
+                    .deleteAll()
+                    .start()
+                }}
+                />
             </div>
         </div>
     )
