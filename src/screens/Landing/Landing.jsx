14d53258be29454d1a15158e490b3856adc9e583
koji <dylanbogerr@outlook.com>
2021-12-17 08:25:54 -0500

    resume updated

--- src/screens/Landing/Landing.jsx
+++ src/screens/Landing/Landing.jsx
@@ -42,7 +42,7 @@ export default function Landing() {
                     <a href="https://twitter.com/dylansqll">
                     <button id="landing-button" type="button" class="btn btn-danger" style={{background: "#D55534"}}>social</button>
                     </a>
-                    <a href="https://drive.google.com/file/d/1zLh1lyi6w3R9ZKdXkOI6ja6yvgkx1ydh/view?usp=sharing">
+                    <a href="https://drive.google.com/drive/folders/1P7o2sIYxjA5_HINynq1hnn31s0XIrdfV?usp=sharing">
                     <button id="landing-button" type="button" class="btn btn-danger" style={{background: "#D55534"}}>resume</button>
                     </a>
                 </div>
